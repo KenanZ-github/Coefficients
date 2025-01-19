dc0c9f03db9474663b2c920af128bf4ae8a9fb23
Kenan igović <zigovic.kenan@gmail.com>
2025-01-19 12:59:02 +0100

    Initial commit

---
+++ src/main/java/com/kenancode/fullstack_backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kenancode.fullstack_backend.repository;
+
+import com.kenancode.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
